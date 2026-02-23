2e5a1d6fd4606b144e06a4b35a0ff32481237c79
mithlesh <kamaldewangan367@gmail.com>
2026-02-23 11:23:31 +0530

    correct lable name in home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import AddNew from "./components/AddNew";
+import Loading from "./components/Loading";
+import NoteType from "./pages/NoteType";
+
 
 function App() {
   return (
     <>
-    <NavBar />
-    <Home />
-    <AddNew />
+      <div className="relative w-screen h-screen bg-[#f6e7d8] overflow-y-hidden">
+        <NavBar />
+        {/* <Home /> */}
+        {/* <Loading /> */}
+        <NoteType />
+        <AddNew />
+      </div>
     </>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="min-h-screen bg-[#f6e7d8] px-4 pb-24 flex flex-col items-center justify-center gap-4 sm:gap-6 md:gap-8">
+    <div className="h-[calc(100vh-4rem)] overflow-hidden bg-[#f6e7d8] px-4 pb-24 flex flex-col items-center justify-center gap-4 sm:gap-6 md:gap-8">
       <div className="w-full max-w-md">
-        <p className="mb-2 text-sm font-medium text-gray-700">Choose Category</p>
+        <p className="mb-2 text-sm font-medium text-gray-700">Select College</p>
         <select
           className="w-full rounded-xl border border-purple-200 bg-white px-4 py-3 text-gray-700 outline-none focus:ring-2 focus:ring-purple-400"
           defaultValue="personal"
@@ -15,7 +15,7 @@ function Home() {
         </select>
       </div>
       <div className="w-full max-w-md">
-        <p className="mb-2 text-sm font-medium text-gray-700">Choose Category</p>
+        <p className="mb-2 text-sm font-medium text-gray-700">Select Course</p>
         <select
           className="w-full rounded-xl border border-purple-200 bg-white px-4 py-3 text-gray-700 outline-none focus:ring-2 focus:ring-purple-400"
           defaultValue="personal"
@@ -26,7 +26,7 @@ function Home() {
         </select>
       </div>
       <div className="w-full max-w-md">
-        <p className="mb-2 text-sm font-medium text-gray-700">Choose Category</p>
+        <p className="mb-2 text-sm font-medium text-gray-700">Select Subject</p>
         <select
           className="w-full rounded-xl border border-purple-200 bg-white px-4 py-3 text-gray-700 outline-none focus:ring-2 focus:ring-purple-400"
           defaultValue="personal"
