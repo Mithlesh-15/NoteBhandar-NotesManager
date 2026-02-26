d176bfee673ccb826b7f909e1a3134f423849190
mithlesh <kamaldewangan367@gmail.com>
2026-02-26 11:11:47 +0530

    add cors in backend and axios in front end

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,24 @@
-import express from 'express';
-import DBConnect from './utils/DBConnect.js';
-import dotenv from 'dotenv';
+import express from "express";
+import DBConnect from "./utils/DBConnect.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 DBConnect();
 const app = express();
 const port = 3000;
 
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
