3215ddc741d8a711cdd03d0b3af1b03c62c9bd6c
mithlesh <kamaldewangan367@gmail.com>
2026-02-27 12:21:03 +0530

    create api templete for get and set

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ import DBConnect from "./utils/DBConnect.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
 import LoginRoute from "./routes/login.route.js";
+import setDataRoute from "./routes/setData.route.js";
+import getDataRoute from "./routes/getData.route.js";
 
 dotenv.config();
 DBConnect();
@@ -22,6 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/login", LoginRoute);
+app.use("/api/v1/set-data", setDataRoute);
+app.use("/api/v1/get-data", getDataRoute);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
