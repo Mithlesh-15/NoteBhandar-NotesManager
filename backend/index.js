defd1dc3ccae193948bca2a12a98e57c364adbb5
mithlesh <kamaldewangan367@gmail.com>
2026-03-07 17:24:23 +0530

    update for vercal

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,8 @@ import jwt from "jsonwebtoken";
 import LoginRoute from "./routes/login.route.js";
 import setDataRoute from "./routes/setData.route.js";
 import getDataRoute from "./routes/getData.route.js";
-import profileRoute from "./routes/profile.route.js"
-import actionRoute from "./routes/action.route.js"
+import profileRoute from "./routes/profile.route.js";
+import actionRoute from "./routes/action.route.js";
 
 dotenv.config();
 DBConnect();
@@ -18,7 +18,7 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://notebhandar.vercel.app",
     credentials: true,
   }),
 );
@@ -32,6 +32,4 @@ app.use("/api/v1/get-data", getDataRoute);
 app.use("/api/v1/profile", profileRoute);
 app.use("/api/v1/action", actionRoute);
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+export default app;
