0759926631a403d162d4feff6a64816d88b09b30
mithlesh <kamaldewangan367@gmail.com>
2026-03-07 14:34:16 +0530

    init nodemailer and write starting code

---
+++ backend/utils/sendMail.js
@@ -0,0 +1,19 @@
+import nodemailer from 'nodemailer'
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.ADMIN_EMAIL,
+    pass: process.env.ADMIN_PASS
+  }
+});
+
+const sendMail = async (subject, message) => {
+  await transporter.sendMail({
+    from: `"NoteBhandar" <${process.env.ADMIN_EMAIL}>`,
+    to: process.env.ADMIN_EMAIL,
+    subject,
+    html: message
+  });
+};
+
+export default sendMail
