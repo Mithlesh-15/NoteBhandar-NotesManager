076af781f0d65dba8b168f4df465e782a702e24e
mithlesh <kamaldewangan367@gmail.com>
2026-03-20 11:39:20 +0530

    add footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { Outlet } from "react-router-dom";
 import AddNew from "./components/AddNew";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="relative w-screen h-screen bg-[#f6e7d8] overflow-y-hidden">
+      <div className="relative flex min-h-screen w-screen flex-col bg-[#f6e7d8]">
         <NavBar />
-        <Outlet />
+        <div className="flex-1 overflow-y-auto pb-6">
+          <Outlet />
+        </div>
+        <Footer />
         <AddNew />
       </div>
     </>
