56b891d134cafc59662d9e1e3757dfc3cd292436
mithlesh <kamaldewangan367@gmail.com>
2026-03-20 11:50:44 +0530

    delete footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { Outlet } from "react-router-dom";
 import AddNew from "./components/AddNew";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <div className="relative flex min-h-screen w-screen flex-col bg-[#f6e7d8]">
+      <div className="relative w-screen h-screen bg-[#f6e7d8] overflow-y-hidden">
         <NavBar />
-        <div className="flex-1 overflow-y-auto pb-6">
-          <Outlet />
-        </div>
-        <Footer />
+        <Outlet />
         <AddNew />
       </div>
     </>
