4ed3cf99da22562d9632083c54fab2c7bf96f696
mithlesh <kamaldewangan367@gmail.com>
2026-03-20 11:31:52 +0530

    error fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -87,7 +87,7 @@ const router = createBrowserRouter(
       <Route
         path="/privacy-policy"
         element={
-         <PrivacyPolicy
+         <PrivacyPolicy />
         }
       />
       <Route
